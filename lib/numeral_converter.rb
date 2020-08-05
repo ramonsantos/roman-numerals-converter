431789acb1aa5c5adae8cd5a3b83292780b825c3
Ramon Santos <ramonsantos.pe@gmail.com>
2020-08-05 12:09:45 -0300

    Update numeral_converter.rb

--- lib/numeral_converter.rb
+++ lib/numeral_converter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'numeral_converter/hindu_arabic_to_roman.rb'
+require_relative 'numeral_converter/hindu_arabic_to_roman'
 require_relative 'numeral_converter/roman_to_hindu_arabic'
 
 module NumeralConverter
